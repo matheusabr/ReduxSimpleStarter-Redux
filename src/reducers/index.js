4b86f0751f10748882410e3ea45f94f3746b9a5b
matheusabreu <matheusabreu.dev@gmail.com>
2018-09-04 19:52:17 -0300

    fix(reducer_active_book): correct import and function closes

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
 import BooksReducer from './reducer_books';
-import ActiveBook from './reducer_active_book.js';
+import ActiveBook from './reducer_active_book';
 
 // Mapping the state
 const rootReducer = combineReducers({
