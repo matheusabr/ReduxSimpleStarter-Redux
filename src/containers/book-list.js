5ee6a0edd733f7151fcc8903f931a19f8a28f171
matheusabreu <matheusabreu.dev@gmail.com>
2018-09-04 17:59:37 -0300

    chore(): connect a Redux state to React component to produce a container

---
+++ src/containers/book-list.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class BookList extends Component {
+  renderList() {
+    return this.props.books.map((book) => {
+      return (
+        <li key={book.title} className="list-group-item">
+          {book.title}
+        </li>
+      );
+    });
+  }
+
+  render() {
+    return (
+      <ul className="list-group col-sm-4">
+        {this.renderList()}
+      </ul>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  // Whateve is returned will show up as this.props
+  // inside of Booklist class
+  return  {
+    books: state.books
+  };
+}
+
+// Export a connection of Redux state + React Component = Container
+export default connect(mapStateToProps)(BookList);
