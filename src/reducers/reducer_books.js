31e8166b26e389d0b0590018bf52b85a7c8fa9ba
matheusabreu <matheusabreu.dev@gmail.com>
2018-09-04 17:30:16 -0300

    chore(): create and combine a Redux reducer

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,10 @@
+// A reducer returns a piece of data
+// to handle a specific scope
+export default function() {
+  return [
+    { title: 'JS' },
+    { title: 'React' },
+    { title: 'Vue' },
+    { title: 'Angular' },
+  ]
+}
